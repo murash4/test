6c297f3e64409737ed9e33064308d3fc7176a9d1
murash4 <qoc306a>
2018-09-18 22:03:35 +0300

    add less compiler

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 var $ = require('jquery');
 var str = require('./constant');
+require('./less');
 
 var App = function() {
-	$('body').html(str);
+	$('body').html('<div class="b-test"><h3 class="b-test__heading">Heading. </h3></div>' + str);
 };
 
 var app = new App();
-$('body').html(app);
+$('body').html(1111);
--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,17 @@ module.exports = {
 		path: __dirname + '/public',
 		filename: 'bundle.js'
 	},
-	mode: 'development'
+	mode: 'development',
+	module: {
+		rules: [{
+			test: /\.less$/,
+			use: [{
+				loader: 'style-loader' // creates style nodes from JS strings
+			}, {
+				loader: 'css-loader' // translates CSS into CommonJS
+			}, {
+				loader: 'less-loader' // compiles Less to CSS
+			}]
+		}]
+	}
 };
